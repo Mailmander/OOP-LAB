1995b89c88da4d7caf21b43155d074b4298736b8
Mailmander <45078861+Mailmander@users.noreply.github.com>
2022-10-26 21:05:11 +0300

    Changes: -Renamed Monopoly (Game Conditions).py to Monopoly_GameConditions.py -Create starter for the Game in the main.py -In Monopoly_GameConditions.py created first classes -Create file for the tests(Tests.py) -Updated Player_class.py

---
+++ Monopoly_GameConditions.py
@@ -0,0 +1,13 @@
+from Player_class import Player
+class GameConditions():
+
+
+	def CreatePlayers(self):
+		self.NumOfPlayers = int(input("Enter number of player.\n"))
+		return
+
+	def GameStart(self):
+		self.CreatePlayers()
+
+	def Step(player):
+		return
---
+++ Player_class.py
@@ -0,0 +1,17 @@
+class Player():
+
+    def __init__(self,playername,playernumber):
+        self.name=playername
+        self.money=20000
+        self.playernumber=playernumber
+
+    def Get_name(self):
+        return self.name
+    def Get_playernumber(self):
+        return self.playernumber
+    def Get_money(self):
+        return self.money
+
+
+
+
