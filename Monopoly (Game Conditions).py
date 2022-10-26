3c230aca94d2de058214630458cf92fdae2ce42d
Lucianian <100424088+Lucianian@users.noreply.github.com>
2022-10-26 20:08:17 +0300

    Add files via upload

---
+++ Monopoly (Game Conditions).py
@@ -0,0 +1,32 @@
+
+class GameConditions:
+
+	FieldSize = 40
+	#StartMoney = 5000
+	PlayersArray = []
+
+
+
+	def __init__(self, NumOfPlayers,):
+		self.NumOfPlayers = NumOfPlayers
+
+	def CreatePlayer():
+		
+
+	def Step(player, )
+
+
+
+
+
+
+
+Monopoly = GameConditions(int(input("Enter number of player.\n")))
+for i in range(0, Monopoly.NumOfPlayers):
+
+
+
+counter = 0;
+while true:
+
+	GameConditions.step()
