212509cfd0a995cce1c2f7c2619fae551b91428e
Mailmander <45078861+Mailmander@users.noreply.github.com>
2022-10-26 22:14:02 +0300

    Changes: -Updated Monopoly_GameConditions.py(Added CreatePlayers) -Updated Player_class.py

--- Monopoly_GameConditions.py
+++ Monopoly_GameConditions.py
@@ -1,10 +1,17 @@
 from Player_class import Player
+
 class GameConditions():
 
+	PlayersArray = {}
 
 	def CreatePlayers(self):
 		self.NumOfPlayers = int(input("Enter number of player.\n"))
+		for i in range(self.NumOfPlayers):
+			self.PlayersArray[i] = Player(i)
+			print(self.PlayersArray[i].name)
+		print(self.PlayersArray[0].name)
 		return
+
 
 	def GameStart(self):
 		self.CreatePlayers()
--- Player_class.py
+++ Player_class.py
@@ -1,16 +1,19 @@
 class Player():
-
-    def __init__(self,playername,playernumber):
-        self.name=playername
-        self.money=20000
-        self.playernumber=playernumber
+    def __init__(self, playernumber):
+        self.name = input("Enter Player name.\n")
+        self.money = 20000
+        self.playernumber = playernumber
 
     def Get_name(self):
         return self.name
+
     def Get_playernumber(self):
         return self.playernumber
+
     def Get_money(self):
         return self.money
+
+
 
 
 
