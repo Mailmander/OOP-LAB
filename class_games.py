593ca5de15e9c76f62825bd1dd4ac05adf7f41a8
sh1d3nk41 <100447186+sh1d3nk41@users.noreply.github.com>
2022-10-26 23:17:36 +0300

    Update class_games.py

--- class_games.py
+++ class_games.py
@@ -6,6 +6,7 @@ class Cell_buyable(Field):
     name = "Default"
     cost = int(0)
     owner = None
+    number = -1
 
 # class Cell_Jail_enter(Field):
 #     def __init__(self):
@@ -16,13 +17,23 @@ class Cell_buyable(Field):
 #         pass
 
 class Cell_buyable_color(Cell_buyable):
+
     color = "Default"
     houses = 0
     houses_cost = 0
     arend = [0, 0, 0, 0, 0, 0]
-    # def cost():
-    #     return self.arend[self.houses]
+    def cost(self):
+        return self.arend[self.houses]
 
-# class Cell_buyable_railway(Cell_buyable):
-#     def __init__(self):
-#         pass
+class Cell_buyable_stations(Cell_buyable):
+    arend_rail = [25, 50, 100, 200]
+
+class Cell_buyable_spec(Cell_buyable):
+    arend_rail = [4, 10]
+    def cost(self, cubics):
+        return self.arend_rail[1] * cubics
+
+class Cell_Chance (Field):
+    def chance(self):
+        return "Тест шанса, как же много надо придумывать, -200iq momment", -200
+
